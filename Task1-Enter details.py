45b35372372736291754d43735b038815fa412c7
AzureAD\SonaliSusmita <jyoti.rmohanty14@gmail.com>
2024-02-05 18:38:00 +0530

    Initial commit

---
+++ Task1-Enter details.py
@@ -0,0 +1,11 @@
+#Take a user input (name, age, roll_number, phone_number) and print the user details.
+
+user_name = input("Enter the name:")
+user_age = int(input("Enter the age:"))
+user_roll_number = int(input("Enter the roll number:"))
+user_phone_number = int(input("Enter the phone number:"))
+print("Enter your name:", user_name)
+print("Enter your age:",user_age)
+print("Enter your roll number", user_roll_number)
+print("Enter your phone number",user_phone_number)
+
---
+++ Task2-Table2.py
@@ -0,0 +1,21 @@
+#Print the Table of 2 by using the command print()
+#2 x 1 = 2
+#2 x 2 = 4
+#...
+#2 x 10 = 20
+print("Table of 2")
+print("2x1 =",2*1)
+print("2x2 =",2*2)
+print("2x3 =",2*3)
+print("2x4 =",2*4)
+print("2x5 =",2*5)
+print("2x6 =",2*6)
+print("2x7 =",2*7)
+print("2x8 =",2*8)
+print("2x9 =",2*9)
+print("2x10 =",2*10)
+
+
+
+
+
